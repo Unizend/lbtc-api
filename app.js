315daeb092683024376536a05218610d07550657
Santiago Rincon <rincorpes@gmail.com>
2019-10-27 11:43:17 -0500

    Testing Payment Methods

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+var request = require('request');
+
+
+// Testing Localbitcoins API Payment Methods
+const payment_methods = request('https://localbitcoins.com/api/payment_methods/', function(err, res, data) {
+
+	console.log(res.statusCode);
+	if (!err && res.statusCode == 200) {
+		console.log(data);
+	}
+});
