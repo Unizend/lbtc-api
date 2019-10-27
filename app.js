0ef74845b094a99d02b185daa57daa0c9de901b8
Santiago Rincon <rincorpes@gmail.com>
2019-10-27 12:06:55 -0500

    get Payment Methods working

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const request = require('request') // To make tests
+const querystring = require('querystring')
+const fetch = require('node-fetch')
+const crypto = require('crypto')
 
 const config = require('./config.json') // Config file
 
@@ -9,6 +12,52 @@ const DEFAULT_HEADERS = {
 	'Content-Type': 'application/x-www-form-urlencoded',
 	'Apiauth-Key': KEY
 }
+
+// This returns a signature for a request as a Base64-encoded string
+const getMessageSignature = (path, params, nonce) => {
+	const postParameters = querystring.stringify(params)
+	const message = nonce + KEY + path + postParameters
+
+	return crypto
+		.createHmac('sha256', SECRET)
+		.update(message)
+		.digest('hex')
+		.toUpperCase()
+}
+
+// This returns http headers
+const getHeaders = (path, params = {}) => {
+	const nonce = new Date() * 1000
+	const signature = getMessageSignature(path, params, nonce)
+
+	return {
+		...DEFAULT_HEADERS,
+		...{ 'Apiauth-Nonce': nonce, 'Apiauth-Signature': signature }
+	}
+}
+
+// Here's where the magic happends
+const api = {
+	get: async path => {
+		const headers = getHeaders(path)
+		const res = await fetch(ROOT_URL + path, { method: 'GET', headers })
+
+		return res.json()
+	}
+}
+
+// Paths
+const paths = {
+	PAYMENT_METHODS: '/api/payment_methods/'
+}
+
+// Test using the api method to get the payment methods.
+const getPaymentMethods = async () => {
+	const response = await api.get(paths.PAYMENT_METHODS)
+	console.log(response)
+}
+
+getPaymentMethods()
 
 // Testing Localbitcoins API Payment Methods
 /*const payment_methods = request('https://localbitcoins.com/api/payment_methods/', function(err, res, data) {
