3c16ccfea4d288a6746aee23552d84604af97383
Santiago Rincon <rincorpes@gmail.com>
2019-10-27 11:54:43 -0500

    config added

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-var request = require('request');
+const request = require('request') // To make tests
 
+const config = require('./config.json') // Config file
+
+const ROOT_URL = 'https://localbitcoins.com'
+const KEY = config.auth.key
+const SECRET = config.auth.secret
+const DEFAULT_HEADERS = {
+	'Content-Type': 'application/x-www-form-urlencoded',
+	'Apiauth-Key': KEY
+}
 
 // Testing Localbitcoins API Payment Methods
-const payment_methods = request('https://localbitcoins.com/api/payment_methods/', function(err, res, data) {
+/*const payment_methods = request('https://localbitcoins.com/api/payment_methods/', function(err, res, data) {
 
 	console.log(res.statusCode);
 	if (!err && res.statusCode == 200) {
 		console.log(data);
 	}
-});
+});*/
