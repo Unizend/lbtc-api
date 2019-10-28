4ada004fe227280983deac65f930827cd815d9c1
Santiago Rincon <rincorpes@gmail.com>
2019-10-28 10:46:57 -0500

    API pahts modified

--- app.js
+++ app.js
@@ -58,30 +58,57 @@ const api = {
 
 // Paths
 const paths = {
-	PAYMENT_METHODS: '/api/payment_methods/',
-	countryPaymentMethods: countrycode => `/api/payment_methods/${countrycode}/`,
-	COUNTRYCODES: '/api/countrycodes/',
-	CURRENCIES: '/api/currencies/',
-	PLACES: '/api/places/', // Not working yet (Missing arguments). TODO: Make it work
-	equation: equation_string => `/api/equation/${equation_string}/`, // TODO: review the localbitcoins equation guide
-	MY_ADS_LIST: '/api/ads/',
-	adById: ad_id => `/api/ad-get/${ad_id}/`,
-	adsListByIds: ads_ids => `/api/ad-get/${ads_ids}/`, // TODO: Make it work
-	updateAd: ad_id => `/api/ad/${ad_id}/`, // TODO
-	createAd: '/api/ad-create/', // TODO
-	updateAdEquation: ad_id => `/api/ad-equation/${ad_id}/`, // TODO
-	removeAd: ad_id => `/api/ad-delate/${ad_id}/` // TODO
-
+	ADS: {
+		my_ads: '/api/ads/',
+		adById: ad_id => `/api/ad-get/${ad_id}/`,
+		adsListByIds: ads_ids => `/api/ad-get/${ads_ids}/`, // TODO: Make it work
+		updateAd: ad_id => `/api/ad/${ad_id}/`, // TODO
+		createAd: '/api/ad-create/', // TODO
+		updateAdEquation: ad_id => `/api/ad-equation/${ad_id}/`, // TODO
+		removeAd: ad_id => `/api/ad-delate/${ad_id}/`, // TODO
+		payment_methods: '/api/payment_methods/',
+		countryPaymentMethods: countrycode => `/api/payment_methods/${countrycode}/`,
+		countrycodes: '/api/countrycodes/',
+		currencies: '/api/currencies/',
+		places: '/api/places/', // Not working yet (Missing arguments). TODO: Make it work
+		equation: equation_string => `/api/equation/${equation_string}/`, // TODO: review the localbitcoins equation guide
+	}
 }
+
+const getMyAds = async () => {
+	const response = await api.get(paths.ADS.my_ads)
+	console.log(response.data.ad_list)
+}
+
+// getMyAds()
+
+const getAdById = async (ad_id) => {
+	const response = await api.get(
+		paths.ADS.adById(ad_id)
+	)
+	console.log(response.data.ad_list)
+}
+
+// getAdById('975638')
+
+// TODO: getAdsListByIds()
+
+// TODO: updateAd()
+
+// TODO: createAd()
+
+// TODO: updateEquation()
+
+// TODO: removeAd()
 
 // Test using the api method to get the payment methods.
 const getPaymentMethods = async (countrycode = null) => {
 	let response = null
 
 	if (countrycode != null) {
-		response = await api.get(paths.countryPaymentMethods(countrycode))
+		response = await api.get(paths.ADS.countryPaymentMethods(countrycode))
 	} else {
-		response = await api.get(paths.PAYMENT_METHODS)
+		response = await api.get(paths.ADS.payment_methods)
 	}
 
 	console.log(response)
@@ -93,7 +120,7 @@ const getPaymentMethods = async (countrycode = null) => {
 
 // Testin countrycodes
 const getCountrycodes = async () => {
-	const response = await api.get(paths.COUNTRYCODES)
+	const response = await api.get(paths.ADS.countrycodes)
 	console.log(response);
 }
 
@@ -101,7 +128,7 @@ const getCountrycodes = async () => {
 
 // Testing Currencies
 const getCurrencies = async () => {
-	const response = await api.get(paths.CURRENCIES)
+	const response = await api.get(paths.ADS.currencies)
 	console.log(response)
 }
 
@@ -122,22 +149,6 @@ const getEquation = async (equation_string = null) => {
 }
 
 // getEquation('btc_in_usd*0.9')
-
-const getMyAds = async () => {
-	const response = await api.get(paths.MY_ADS_LIST)
-	console.log(response)
-}
-
-// getMyAds()
-
-const getAdById = async (ad_id) => {
-	const response = await api.get(
-		paths.adById(ad_id)
-	)
-	console.log(response.data.ad_list)
-}
-
-// getAdById('975638')
 
 // Testing Localbitcoins API Payment Methods
 /*const payment_methods = request('https://localbitcoins.com/api/payment_methods/', function(err, res, data) {
